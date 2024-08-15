eee074e610918785c82c54d39f61ce4452f58678
mijimenezl <167145289+mijimenezl@users.noreply.github.com>
2024-08-15 10:16:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ st.write("recuerdo el día que de la chamba me enamoré")
 
 image = Image.open('The Dark Lord.png')
 st.image(image, caption='the dark lord')
+
+texto = st.text_input('Escribele algo', 'Este es mi texto')
+st.write('El texto escirto es', texto)
