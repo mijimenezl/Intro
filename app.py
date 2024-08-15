ad9560c66b1914c070d6f5f2d42df8fbfc623def
mijimenezl <167145289+mijimenezl@users.noreply.github.com>
2024-08-15 10:26:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,23 @@ st.image(image, caption='the dark lord')
 
 texto = st.text_input('Escribele algo', 'Este es mi texto')
 st.write('El texto escirto es', texto)
+
+st.subheader ("Ahora usemos 2 Columnas")
+
+col1, col2 = st.columns(2)
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+  resp = st.checkboc ('Estoy de acuerdo')
+  if resp: 
+    st.write('Correcto!')
+
+with col2: 
+  st.subheader("Esta es la segunda columna")
+  modo = st.radio("Quien es Darth Vader", ('Anakin', 'Obiwan', 'Dooku'))
+  if modo == 'Anakin':
+    st.write('Correcto, Darth Vader es el caballero Jedi Anakin Skywalker')
+  if modo == 'Obiwan':
+    st.write('Darth Vader es el caballero Jedi Anakin Skywalker')
+  if modo == 'Dooku':
+    st.write('Darth Vader es el caballero Jedi Anakin Skywalker')
