d0beb08ccf6695cbf7b1346f6ef36d34e1bb3514
mijimenezl <167145289+mijimenezl@users.noreply.github.com>
2024-08-15 10:05:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st 
+from PIL import Image
+
 st.title("Mi primera página")
 st.header("mi primera chamba")
 st.write("recuerdo el día que de la chamba me enamoré")
+
+image = Image.open('The Dark Lord.png')
